63e0bdb4c2b2ab83949df41a5fb3b95a40a64388
LuQ232 <piotrbednarek99@gmail.com>
2020-09-04 20:25:48 +0200

    Polimorfizm

---
+++ Polimorfizm/Polimorfizm.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+class Ojciec
+{
+public:
+    virtual void funkcja() = 0;
+};
+
+class Pierwszy_potomek:public Ojciec
+{
+public:
+    void funkcja()
+    {
+        std::cout << "Jest to funkcja pierwszego potomka\n";
+    }
+};
+
+class Drugi_potomek :public Ojciec
+{
+public:
+    void funkcja()
+    {
+        std::cout << "Jest to funkcja drugiego potomka\n";
+    }
+};
+
+int main()
+{
+    Pierwszy_potomek pierwszy;
+    Drugi_potomek drugi;
+    Ojciec* wirtualny_wskaznik; //tworzymy wirtualny wskaznik
+    wirtualny_wskaznik = &pierwszy; // ustawiamy wskaznik na obiekt pierwszego potomka
+    wirtualny_wskaznik->funkcja();  // wywolujemy funkcje
+    wirtualny_wskaznik = &drugi;    // ustawiamy wskaznik na obiekt pierwszego potomka
+    wirtualny_wskaznik->funkcja();  // wywolujemy funkcje
+}
